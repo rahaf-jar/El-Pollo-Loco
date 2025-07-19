18d6702cc0f918411e5adf71b3fde0f8e277ad37
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-07-19 22:07:13 +0200

    inserted imgs

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,5 +1,9 @@
 class Character extends MoveableObject {
 
+    constructor() {
+        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+    }
+
     jump() {
         console.log("Character jumps");
     }   
