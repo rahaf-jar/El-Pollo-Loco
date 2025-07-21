ef71c7cf693399a39779c425de0aaabda9d26da9
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-07-21 15:26:20 +0200

    show character and enemies

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,10 +1,10 @@
 class Character extends MoveableObject {
-
     constructor() {
-        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        super();
+        this.loadImage('img/2_character_pepe/1_idle/idle/I-1.png'); // ✅ then load image
     }
 
     jump() {
         console.log("Character jumps");
-    }   
+    }
 }
--- classes/moveable-object.class.js
+++ classes/moveable-object.class.js
@@ -1,16 +1,17 @@
 class MoveableObject{
     x = 120;
-    y = 400;
+    y = 250;
     img;
+    height = 200;
+    width = 100;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
-    
+
     moveRight(){
         console.log("Move right");
-        
     }
 
     moveLeft() {
