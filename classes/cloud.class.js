25197690f8e29e781a6f90f3a43744d2a0b5d9c0
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-07-21 22:27:37 +0200

    made clouds moveable

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,10 +1,18 @@
 class Cloud extends MoveableObject {
-    y = 20;
-    height = 250;
-    width = 500;
+  y = 20;
+  height = 250;
+  width = 750;
 
-  constructor() {
-    super().loadImage('img/5_background/layers/4_clouds/1.png');
+  constructor(imagePath) {
+    super().loadImage(imagePath);
     this.x = Math.random() * 500;
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.20;
+    }, 1000 / 60);
   }
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,7 +1,10 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Cloud()];
+  clouds = [
+    new Cloud('img/5_background/layers/4_clouds/2.png'),
+    new Cloud('img/5_background/layers/4_clouds/1.png'),
+  ];
   backgroundObjects = [
     new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),        
