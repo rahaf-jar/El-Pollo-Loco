eddfec585802289c8eacd01fbe752e565c271513
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-07-23 00:28:30 +0200

    move world/backgrounds

--- classes/world.class.js
+++ classes/world.class.js
@@ -6,37 +6,63 @@ class World {
     new Cloud("img/5_background/layers/4_clouds/1.png"),
   ];
   backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/air.png", -719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
+
     new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 0),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 0),
     new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 0),
     new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+
+    new BackgroundObject("img/5_background/layers/air.png", 719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+
+    new BackgroundObject("img/5_background/layers/air.png", 719*2),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719*2),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719*2),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719*2),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719*2),
+
+    new BackgroundObject("img/5_background/layers/air.png", 719*3),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719*3),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719*3),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719*3),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719*3),
   ];
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
-    this.keyboard = keyboard
+    this.keyboard = keyboard;
     this.draw();
     this.setWorld();
   }
 
-  setWorld(){
+  setWorld() {
     this.character.world = this;
   }
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
+
     this.addObjectsToMap(this.backgroundObjects);
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     let self = this;
     requestAnimationFrame(() => self.draw());
