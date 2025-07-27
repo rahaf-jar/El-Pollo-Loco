7d686e9ec0047139e4f10f50c99774f52b8d8652
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-07-27 18:38:26 +0200

    set basic statusbar logic

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -1,3 +1,38 @@
-class StatusBar{
-    
+class StatusBar extends DrawAbleObject {
+  images = [
+    "img/7_statusbars/1_statusbar/2_statusbar_health/orange/0.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/orange/20.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/orange/40.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/orange/60.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/orange/80.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/orange/100.png",
+  ];
+
+  percentage = 100;
+
+  constructor() {
+    this.loadImages(this.images);
+  }
+
+  setPercentage() {
+    this.percentage = percentage;
+    let ImagePath = this.images[this.resolveImageIndex()];
+    this.img = this.imageCache[ImagePath];
+  }
+
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
