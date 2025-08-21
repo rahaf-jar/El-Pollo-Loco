05d1d25db53f9b7ff4df84d65b1abf58ad1d1756
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-08-21 19:46:41 +0200

    updated character statusbar logic so when it is empty character dies

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -45,13 +45,13 @@ class StatusBar extends DrawAbleObject {
   resolveImageIndex() {
     if (this.percentage === 100) {
       return 5;
-    } else if (this.percentage > 80) {
-      return 4;
     } else if (this.percentage > 60) {
-      return 3;
+      return 4;
     } else if (this.percentage > 40) {
-      return 2;
+      return 3;
     } else if (this.percentage > 20) {
+      return 2;
+    } else if (this.percentage > 0) {
       return 1;
     } else {
       return 0;
